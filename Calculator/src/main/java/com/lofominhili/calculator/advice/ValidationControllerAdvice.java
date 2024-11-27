348702938d049cc2d613dd313713fae07b5bb0b1
daniel <kontoshin96@mail.ru>
2024-11-27 14:46:14 +0300

    Imports optimized and fixed bug

--- Calculator/src/main/java/com/lofominhili/calculator/advice/ValidationControllerAdvice.java
+++ Calculator/src/main/java/com/lofominhili/calculator/advice/ValidationControllerAdvice.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
